ee4ba2b63f8c7e33d96f671f1c2e7e91b0de7905
RoAlencar <roalencarx@gmail.com>
2022-06-03 02:35:45 -0300

    [LP4] initial commit

---
+++ vagas/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'ads/lpiv',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '8c22c6ebcb1141d124e2740df43fa99c9c0c3720',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'ads/lpiv' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '8c22c6ebcb1141d124e2740df43fa99c9c0c3720',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
