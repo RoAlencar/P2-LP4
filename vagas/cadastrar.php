cad03c98e4559e93c58860420935455b2a09e97a
RoAlencar <roalencarx@gmail.com>
2022-06-03 11:48:15 -0300

    Inserindo validação do Post

--- vagas/cadastrar.php
+++ vagas/cadastrar.php
@@ -1,6 +1,11 @@
 <?php
 require __DIR__.'/vendor/autoload.php';
 
+//VALIDAÇÃO DO POST
+if(isset($_POST['titulo'],$_POST['descricao'],$_POST['ativo'])){
+    die('Cadastrar');
+}
+
 include __DIR__.'/includes/header.php';
 include __DIR__.'/includes/listagem.php';
 include __DIR__.'/includes/footer.php';
