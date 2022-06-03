0d18cffd429e37815965740da5099a8df29f2dbc
RoAlencar <roalencarx@gmail.com>
2022-06-03 11:44:24 -0300

    criando campos no formulario

--- vagas/cadastrar.php
+++ vagas/cadastrar.php
@@ -2,5 +2,5 @@
 require __DIR__.'/vendor/autoload.php';
 
 include __DIR__.'/includes/header.php';
-include __DIR__.'/includes/formulario.php';
+include __DIR__.'/includes/listagem.php';
 include __DIR__.'/includes/footer.php';
